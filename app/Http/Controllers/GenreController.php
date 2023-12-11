16f8e3d3fda50c2fe7fd69ea40e532f033e37a21
Andrei Trifu <Andrei.Trifu84@gmail.com>
2023-12-11 11:04:50 +0000

    created get all genres controller

---
+++ app/Http/Controllers/GenreController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    private Genre $genre;
+
+    public function __construct(Genre $genre) 
+    {
+        $this->genre=$genre;
+    }
+
+    public function getAllGenres(Genre $genre) 
+    {
+        $genres = Genre::all(['id', 'name']);
+
+        return response()->json([
+            'data'=> $genres,
+            'message'=> 'Genres retrived', 
+        ], 200);
+    }
+
+
+
+
+}
