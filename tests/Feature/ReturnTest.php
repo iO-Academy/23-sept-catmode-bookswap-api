64191f90bbcef785829dfc03ba3cb5e1936a5276
Kellie Marsh <miss.kellie.marsh@gmail.com>
2023-12-12 16:35:09 +0000

    Return a book route tests added

--- tests/Feature/ReturnTest.php
+++ tests/Feature/ReturnTest.php
@@ -27,6 +27,67 @@ class ReturnTest extends TestCase
                 
     } 
 
+    public function test_return_book_by_id_invalid_book_not_found(): void
+    {
+        $response = $this->putJson('/api/books/return/5', ['email'=>"test@test.com"]);
+
+        $response->assertStatus(404)
+                 ->assertJson([
+                    'message' => 'Book 5 was not found'
+                 ]);
+                
+    } 
+
+    public function test_return_book_by_id_invalid_book_not_claimed(): void
+    {
+       
+        $claim = Claim::factory()->create();
+        $book = Book::factory()->create();
+
+        $book->claimed_by_name = '';
+        $claim->book_id = $book->id;
+
+        $claim->save();
+        $book->save();
+    
+        $response = $this->putJson('/api/books/return/1', ['email'=>"$claim->email"]);
+
+        $response->assertStatus(400)
+                 ->assertJson([
+                    'message' => 'Book 1 is not currently claimed'
+                 ]);
+                
+    } 
+
+    public function test_return_book_by_id_invalid_email(): void
+    {
+        $claim = Claim::factory()->create();
+
+        $response = $this->putJson('/api/books/return/1', ['email'=>"test123@test.com"]);
+
+        $response->assertStatus(400)
+                 ->assertJson([
+                    'message' => 'Book 1 was not returned. test123@test.com did not claim this book.'
+                 ]);
+                
+    } 
+
+
+    public function test_return_book_by_id_invalid_no_email(): void
+    {
+        $claim = Claim::factory()->create();
+
+        $response = $this->putJson('/api/books/return/1', ['email'=>""]);
+
+        $response->assertStatus(422)
+                 ->assertJson([
+                    'message' => 'The email field is required.'
+                 ]);
+                
+    } 
+
+
+
     // public function test_return_book_by_id_server_error(): void
     // {
     //     Claim::factory()->create();
