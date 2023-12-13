6088820834f398960add5dcba5b2eae85fe53d83
Sophie Preston <sophkitty1986+gh@gmail.com>
2023-12-13 14:04:50 +0000

    amended error on migration file

--- database/migrations/2023_12_13_103101_on_books_table_make_claimed_by_name_allow_null.php
+++ database/migrations/2023_12_13_103101_on_books_table_make_claimed_by_name_allow_null.php
@@ -17,7 +17,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('books', function (Blueprint $table) {
-            $table->rollback('claimed_by_name')->nullable(false)->change();
+            $table->string('claimed_by_name')->nullable(false)->change();
         });
     }
 };
