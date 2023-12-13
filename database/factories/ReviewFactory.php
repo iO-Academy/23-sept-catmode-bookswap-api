687f7fe11c4d5b6f155429448cac4355ec0c42bf
Henry Meager <henry.meager@gmail.com>
2023-12-13 10:55:00 +0000

    completed story10, all tests are completed

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name'=> $this->faker->text(10),
+            'rating'=> rand(0,5),
+            'review'=> $this->faker->text(200),
+            'book_id'=> Book::factory()
         ];
     }
 }
